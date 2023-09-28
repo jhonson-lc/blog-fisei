793a8e3613a185317b1fc3c0bb2a87185a63848e
jhonson-lc <jhoncisneros11@gmail.com>
2023-09-28 09:40:55 -0500

    feat(apps/client): Add icons

---
+++ apps/client/src/app/icons.js
@@ -0,0 +1,33 @@
+export const icons = {
+  ChevronLeft: (
+    <svg
+      fill="none"
+      height="24"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+      <path d="M15 6l-6 6l6 6" />
+    </svg>
+  ),
+  Home: (
+    <svg
+      fill="none"
+      height={16}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      width={16}
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M5 12H3l9-9 9 9h-2M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-7" />
+      <path d="M10 12h4v4h-4z" />
+    </svg>
+  ),
+};
