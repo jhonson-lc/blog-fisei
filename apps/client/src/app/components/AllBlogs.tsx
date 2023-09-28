acd7122ab7be41c98ae09aa6052b88fd3c468190
jhonson-lc <jhoncisneros11@gmail.com>
2023-09-28 09:41:26 -0500

    feat(apps/client): Add page of all blogs

--- apps/client/src/app/components/AllBlogs.tsx
+++ apps/client/src/app/components/AllBlogs.tsx
@@ -1,68 +1,77 @@
+"use client";
 import { Blog, getAvatar, getImage } from "@/lib/blogs";
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
 import Avatar from "./Avatar";
+import WrapperBlogs from "./WrapperBlogs";
+import { readingTimeOfBlog } from "../utils/helpers";
+import { useRouter } from "next/navigation";
 
 interface Props {
   blogs: Blog["attributes"][];
 }
 
 const AllBlogs: React.FC<Props> = ({ blogs }) => {
-  console.log(blogs);
+  const router = useRouter();
   return (
     <section className="my-6">
-      <div className="relative mb-8">
-        <h2 className="text-5xl z-50 font-bold">Nuevos blogs</h2>
-        <span className="absolute opacity-10 -z-50 top-1/2 -translate-y-1/2 -left-6 text-8xl font-bold">
-          Blogs
-        </span>
+      <div className="relative mb-16">
+        <h2 className="text-5xl z-50 font-bold text-neutral-100 dark:text-neutral-900">
+          Nuevos blogs
+        </h2>
       </div>
-      <article className="grid grid-cols-3 gap-4 w-full">
+      <WrapperBlogs>
         {blogs.map((blog) => (
-          <article key={blog.slug} className="py-4 relative">
-            <div className="relative w-full h-60">
-              <Image
+          <article
+            key={blog.slug}
+            className="w-full h-full relative flex flex-col group image-gallery cursor-pointer"
+            onClick={() => router.push(`/blog/${blog.slug}`)}
+          >
+            <div className="relative flex-1 w-full h-px">
+              <img
                 alt={blog.title}
-                className="rounded-2xl object-cover"
-                fill={true}
+                className="rounded-2xl object-cover w-full h-full border-white/10 dark:border-black/10 border-2"
                 src={getImage({ cover: blog.cover })}
               />
-              <Link
-                className="absolute bg-white gap-0 rotate-45 top-4 right-4 text-8xl text-black inline-flex items-center justify-center h-10 w-10 rounded-full hover:scale-125 transition-transform cursor-pointer"
-                href={`/blog/${blog.slug}`}
-              >
+              <span className="absolute bg-amber-400 group-hover:bg-amber-500 gap-0 rotate-180 top-4 right-4 text-8xl text-neutral-100 dark:text-neutral-900 inline-flex items-center justify-center h-10 w-10 rounded-full group-hover:scale-125 transition-transform cursor-pointer">
                 <svg
                   fill="none"
-                  height="24"
+                  height={24}
                   stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  width="24"
+                  strokeWidth={2}
+                  width={24}
                 >
-                  <path d="M0 0h24v24H0z" fill="none" stroke="none" />
-                  <path d="M12 5l0 14" />
-                  <path d="M16 9l-4 -4" />
-                  <path d="M8 9l4 -4" />
+                  <path d="M0 0h24v24H0z" stroke="none" />
+                  <path d="M17 7 7 17M16 17H7V8" />
                 </svg>
-              </Link>
+              </span>
             </div>
-            <div className="p-4 justify-between flex flex-col">
-              <div className="flex flex-col">
-                <h2 className={`mb-1 text-2xl font-semibold`}>{blog.title}</h2>
-                <p className={`text-base mb-6 opacity-50 line-clamp-2`}>{blog.description}</p>
-              </div>
+            <div className="p-4 justify-evenly flex flex-col">
+              <span className="text-[10px] font-medium mr-2 py-0.5 rounded text-amber-500">
+                {blog.category.data.attributes.name}
+                {" • "}
+                {readingTimeOfBlog(blog.blocks).readingTime} min
+              </span>
+              <h2
+                className={`mb-1 text-xl font-medium text-neutral-200 group-hover:text-neutral-100 group-hover:dark:text-neutral-950 dark:text-neutral-900 transition-colors`}
+              >
+                {blog.title}
+              </h2>
+              <p
+                className={`text-sm mb-6 opacity-50 line-clamp-2 text-neutral-100 dark:text-neutral-900`}
+              >
+                {blog.description}
+              </p>
               <Avatar
                 name={blog.author.data.attributes.name}
                 picture={getAvatar({ author: blog.author })}
-                publishedAt={new Date()}
+                publishedAt={new Date(blog.publishedAt)}
               />
             </div>
           </article>
         ))}
-      </article>
+      </WrapperBlogs>
     </section>
   );
 };
---
+++ apps/client/src/app/components/WrapperBlogs.tsx
@@ -0,0 +1,38 @@
+"use client";
+import styled from "@emotion/styled";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Blogs = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  gap: 15px;
+  grid-auto-rows: 350px;
+  grid-auto-flow: dense;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+
+  .image-gallery:nth-of-type(5n - 4) {
+    grid-column: span 2;
+  }
+  .image-gallery:nth-of-type(5n - 3) {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+  .image-gallery:nth-of-type(5n - 1) {
+    grid-column: span 1;
+  }
+
+  @media (max-width: 768px) {
+    gap: 15px 0;
+  }
+`;
+
+const WrapperBlogs: React.FC<Props> = ({ children }) => {
+  return <Blogs>{children}</Blogs>;
+};
+
+export default WrapperBlogs;
